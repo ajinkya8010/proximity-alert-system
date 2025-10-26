e2096b6de75f694d87ccffb8ce77b19bdcfa4294
ajinkya8010 <ajinkya8975@gmail.com>
2025-10-26 22:28:41 +0530

    -

--- frontend/src/lib/apiRequest.js
+++ frontend/src/lib/apiRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:3001/api",
-  withCredentials: true,
+    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3001/api",
+    withCredentials: true,
 });
 
 export default apiRequest;
